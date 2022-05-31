a90fadcddc5612d256eb43996a3f377a20aaa1b2
darioka <cannone.dario@gmail.com>
2022-05-31 18:09:56 +0100

    just black

--- impactdeal/cleaning.py
+++ impactdeal/cleaning.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.base import TransformerMixin, BaseEstimator
+from sklearn.utils.validation import check_is_fitted
+
+from .config.column_names import NUMERICAL, CATEGORICAL, DATES
+
 
 def normalize_missing(df, columns):
     """
@@ -19,21 +24,24 @@ def normalize_missing(df, columns):
 
     df (pd.DataFrame)
     """
-    
+
     missing_patterns = [
-        'invalid!',
-        'no data!',
-        'nodata!',
-        'not defined',
-        'unknown',
-        'sap05',
+        "invalid!",
+        "no data!",
+        "nodata!",
+        "not defined",
+        "unknown",
+        "sap05",
     ]
-    
+
     new_df = df.copy()
-    
+
     new_df[columns] = new_df[columns].applymap(
-        lambda x: np.nan if any([str(x).lower().startswith(pattern) for pattern in missing_patterns]) else x)
-    
+        lambda x: np.nan
+        if any([str(x).lower().startswith(pattern) for pattern in missing_patterns])
+        else x
+    )
+
     return new_df
 
 
@@ -51,10 +59,9 @@ def clean_age_band(df):
 
     df (pd.DataFrame)
     """
-    
 
     df = df.copy()
-   
+
     def _clean(x):
         x = str(x).replace("England and Wales: ", "")
 
@@ -90,9 +97,10 @@ def clean_age_band(df):
             return "2003-2006"
         else:
             return "2007 onwards"
-        
+
     df.loc[~df["CONSTRUCTION_AGE_BAND"].isnull(), "CONSTRUCTION_AGE_BAND"] = df.loc[
-        ~df["CONSTRUCTION_AGE_BAND"].isnull(), "CONSTRUCTION_AGE_BAND"].apply(_clean)
+        ~df["CONSTRUCTION_AGE_BAND"].isnull(), "CONSTRUCTION_AGE_BAND"
+    ].apply(_clean)
     return df
 
 
@@ -110,7 +118,7 @@ def clean_floor_level(df):
 
     df (pd.DataFrame)
     """
-    
+
     def _clean(x):
         if x == "21st or above":
             return "above 20th"
@@ -118,7 +126,7 @@ def clean_floor_level(df):
             x = int(str(x)[:2])
         except ValueError:
             return x
-        
+
         x = str(x)
         if x in ["0", "-1"]:
             return x
@@ -132,11 +140,15 @@ def clean_floor_level(df):
             return x + "th"
 
     df = df.copy()
-    
-    df["FLOOR_LEVEL"] = df["FLOOR_LEVEL"].str.strip().str.lower().replace(
-        {"ground": 0, "ground floor": 0, "-1": "basement"}).apply(_clean)
-    
-    
+
+    df["FLOOR_LEVEL"] = (
+        df["FLOOR_LEVEL"]
+        .str.strip()
+        .str.lower()
+        .replace({"ground": 0, "ground floor": 0, "-1": "basement"})
+        .apply(_clean)
+    )
+
     return df
 
 
@@ -160,8 +172,136 @@ def clean_mainheat(df):
             return str(int(x))
         except ValueError:
             return np.nan
-    
+
     df.copy()
     df["MAIN_HEATING_CONTROLS"] = df["MAIN_HEATING_CONTROLS"].apply(_clean)
-    
+
     return df
+
+
+class Cleaner(TransformerMixin, BaseEstimator):
+    """
+    Clean the EPC rating dataset
+
+    The input to this transformer must be a pandas.DataFrame.
+
+    Parameters
+    ----------
+
+    text_features: bool, default=False
+        Whether to keep text columns
+    
+    missing_threshold: float, default=0.7
+        Threshold of percentage of missing values in each feature
+        beyond which the feature will be discarderd.
+
+    Attributes
+    ----------
+
+    feature_names_in_: list of strings
+        Names of the input columns
+    
+    feature_names_out_: list of strings
+        Names of the output columns
+    """
+
+    _numerical_features = NUMERICAL
+    _categorical_features = [
+        x for x in CATEGORICAL if not x.lower().endswith("description")
+    ]
+    _text_features = [x for x in CATEGORICAL if x.lower().endswith("description")]
+
+    def __init__(self, text_features=False, missing_threshold=0.7):
+        if text_features in [True, False]:
+            self.text_features = text_features
+        else:
+            raise ValueError("text_features must be either True or False")
+
+        if (
+            np.asarray(missing_threshold).dtype.kind in ["i", "f"]
+            and missing_threshold > 0
+            and missing_threshold <= 1
+        ):
+            self.missing_threshold = missing_threshold
+        else:
+            raise ValueError("missing threshold must be 0 < x <= 1")
+
+    def fit(self, X, y=None):
+        """
+        Fit the Cleaner to X. Compute percentages of missing values
+        and choose the columns to keep.
+
+        Parameters
+        ----------
+
+        X : pd.DataFrame
+
+        y: None
+            Ignored. This parameter exists only for compatibility with
+            :class:`~sklearn.pipeline.Pipeline`.
+        """
+        X = self._check_X(X)
+
+        X = normalize_missing(X, self._categorical_features)
+        if self.text_features:
+            X = normalize_missing(X, self._text_features)
+
+        # drop where too many missing
+        columns_to_drop = [
+            i for i, x in X.isnull().mean().iteritems() if x > self.missing_threshold
+        ]
+        # drop variables with informative content similar to other features
+        columns_to_drop += [
+            "FIXED_LIGHTING_OUTLETS_COUNT",
+            "LOW_ENERGY_FIXED_LIGHT_COUNT",
+        ]
+
+        self.feature_names_in_ = X.columns.tolist()
+        self.feature_names_out_ = [x for x in X.columns if x not in columns_to_drop]
+
+        return self
+
+    def _check_X(self, X):
+        # accept dataframes only
+        if not (hasattr(X, "iloc") and getattr(X, "ndim", 0) == 2):
+            raise ValueError("X must be a pandas dataframe")
+
+        all_features = self._numerical_features + self._categorical_features
+        if self.text_features:
+            all_features += self._text_features
+
+        try:
+            cols = X[all_features].columns
+        except KeyError:
+            raise KeyError("X must have all NUMERICAL and CATEGORICAL features")
+
+        if len(cols) != len(all_features):
+            raise KeyError("X has duplicated columns")
+
+        return X.loc[:, all_features]
+
+    def transform(self, X, y=None):
+        """
+        Apply custom cleaning functions to specific columns.
+        Filter out columns with too many missings.
+
+        Parameters
+        ----------
+
+        X : pd.DataFrame
+
+        y: None
+            Ignored. This parameter exists only for compatibility with
+            :class:`~sklearn.pipeline.Pipeline`.
+        """
+
+        check_is_fitted(self, ["feature_names_out_"])
+
+        X = self._check_X(X)
+        X = normalize_missing(X, self._categorical_features)
+        X = clean_age_band(X)
+        X = clean_floor_level(X)
+        X = clean_mainheat(X)
+        X = X.loc[:, self.feature_names_out_]
+
+        return X
--- impactdeal/preprocessing.py
+++ impactdeal/preprocessing.py
@@ -4,7 +4,12 @@ from warnings import warn
 
 from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.utils.validation import check_is_fitted
-from sklearn.utils import check_array
+from sklearn.utils import check_X_y, check_array
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LinearRegression, PoissonRegressor
+from sklearn.compose import TransformedTargetRegressor
+from sklearn.preprocessing import PowerTransformer
+from sklearn.pipeline import make_pipeline
 
 
 class CategoryReducer(TransformerMixin, BaseEstimator):
@@ -34,7 +39,6 @@ class CategoryReducer(TransformerMixin, BaseEstimator):
         the features in X and corresponding with the output of ``transform``).
     """
 
-
     def __init__(self, size=10, encoded_value=np.nan):
 
         self._validate_size(size)
@@ -46,7 +50,9 @@ class CategoryReducer(TransformerMixin, BaseEstimator):
 
         size_dtype = np.asarray(size).dtype.kind
 
-        if (size_dtype == "i" and size <= 0) or (size_dtype == "f" and (size <= 0 or size >= 1)):
+        if (size_dtype == "i" and size <= 0) or (
+            size_dtype == "f" and (size <= 0 or size >= 1)
+        ):
             raise ValueError(
                 "size={0} should be either positive or a float in the "
                 "(0, 1) range".format(size)
@@ -54,7 +60,7 @@ class CategoryReducer(TransformerMixin, BaseEstimator):
 
         if size_dtype not in ("i", "f"):
             raise ValueError("Invalid value for size: {}".format(size))
-    
+
     def fit(self, X, y=None):
         """
         Fit the CategoryReducer to X, computing counts and frequencies.
@@ -73,11 +79,14 @@ class CategoryReducer(TransformerMixin, BaseEstimator):
 
         n_samples, n_features = X.shape
 
-        self.value_counts_ = {col: X[col].value_counts().reset_index().rename(columns={col: "count"}) for col in X}
+        self.value_counts_ = {
+            col: X[col].value_counts().reset_index().rename(columns={col: "count"})
+            for col in X
+        }
         for col, df in self.value_counts_.items():
             df["freq"] = df["count"] / n_samples
             df.set_index("index", drop=True, inplace=True)
-        
+
         return self
 
     def _check_X(self, X):
@@ -85,12 +94,13 @@ class CategoryReducer(TransformerMixin, BaseEstimator):
         # accept dataframes only
         if not (hasattr(X, "iloc") and getattr(X, "ndim", 0) == 2):
             raise ValueError("X must be a pandas dataframe")
-        
+
         return pd.DataFrame(
             check_array(X, dtype="object", force_all_finite=False),
-            columns = X.columns, index=X.index
+            columns=X.columns,
+            index=X.index,
         )
-    
+
     def transform(self, X, y=None):
         """
         Encoded all rare categories in X with the encoded value.
@@ -113,5 +123,123 @@ class CategoryReducer(TransformerMixin, BaseEstimator):
         for col in self.value_counts_:
             mapping = X[col].map(self.value_counts_[col][threshold_type])
             X[col] = X[col].mask(mapping < self.size, self.encoded_value)
-        
+
         return X
+
+
+class RoomsImputer(TransformerMixin, BaseEstimator):
+    """
+    Use linear regressions on TOTAL_FLOOR_AREA to input missing values in
+    NUMBER_HABITABLE_ROOMS and NUMBER_HEATED_ROOMS.
+
+    The input to this transformer must be a pandas.DataFrame.
+    Output is a dataframe with NUMBER_HABITABLE_ROOMS and NUMBER_HEATED_ROOMS.
+    
+    Attributes
+    ----------
+
+    feature_names_in_: list of strings
+        ["TOTAL_FLOOR_AREA", "NUMBER_HABITABLE_ROOMS", "NUMBER_HEATED_ROOMS"]
+    
+    feature_names_out_: list of strings
+        ["NUMBER_HABITABLE_ROOMS", "NUMBER_HEATED_ROOMS"]
+    
+    imputers_: dict
+        Contains the sklearn models for each feature
+    """
+
+    _x_cols = ["TOTAL_FLOOR_AREA"]
+    _y_cols = ["NUMBER_HABITABLE_ROOMS", "NUMBER_HEATED_ROOMS"]
+
+    def fit(self, X, y=None):
+        """
+        Fit multiple linear regressions on TOTAL_FLOOR_AREA to
+        predict NUMBER_HABITABLE_ROOMS and NUMBER_HEATED_ROOMS,
+        then compute cross-validated scores.
+
+        The model is a linear regression with log-transformed target
+        and power-transformed feature.
+
+        Parameters
+        ----------
+
+        X : pd.DataFrame
+
+        y: None
+            Ignored. This parameter exists only for compatibility with
+            :class:`~sklearn.pipeline.Pipeline`.
+        """
+        x, ys = self._check_X(X)
+        self.feature_names_in_ = self._x_cols + self._y_cols
+        self.feature_names_out_ = self._y_cols
+
+        self.imputers_ = {}
+        self.mean_absolute_error_ = {}
+        for i, y_col in enumerate(self._y_cols):
+            pt = PowerTransformer()
+            tt = TransformedTargetRegressor(
+                regressor=LinearRegression(),
+                func=lambda x: np.log(1 + x),
+                inverse_func=lambda x: np.exp(x) - 1,
+            )
+            pipeline = make_pipeline(pt, tt)
+
+            pipeline.fit(x, ys[:, i])
+            self.imputers_[y_col] = pipeline
+
+    def _check_X(self, X, dropna=True):
+        # accept dataframes only
+        if not (hasattr(X, "iloc") and getattr(X, "ndim", 0) == 2):
+            raise ValueError("X must be a pandas dataframe with 3 columns")
+
+        try:
+            x = X.loc[:, self._x_cols + self._y_cols]
+        except KeyError:
+            raise ValueError(
+                f"X must have the following columns: {self._x_cols + self._y_cols}"
+            )
+
+        if dropna:
+            x = x.dropna()
+        ys = x.loc[:, self._y_cols]
+        x = x.drop(columns=self._y_cols)
+
+        x = check_array(x, dtype="numeric", force_all_finite=dropna)
+        ys = np.stack(
+            [
+                check_array(
+                    ys[col], force_all_finite=dropna, ensure_2d=False, dtype="numeric"
+                )
+                for col in ys
+            ],
+            axis=-1,
+        )
+        return x, ys
+
+    def transform(self, X, y=None):
+        """
+        Replace missing values in X with predictions from the fitted models.
+
+        Output only ["NUMBER_HABITABLE_ROOMS", "NUMBER_HEATED_ROOMS"]
+        independently from the additional columns that may be present in X.
+
+        Parameters
+        ----------
+
+        X : pd.DataFrame
+
+        y: None
+            Ignored. This parameter exists only for compatibility with
+            :class:`~sklearn.pipeline.Pipeline`.
+        """
+        check_is_fitted(self, ["imputers_"])
+        x, ys = self._check_X(X, dropna=False)
+
+        ys_pred = pd.DataFrame(ys, index=X.index, columns=self._y_cols)
+        for i, y_col in enumerate(self._y_cols):
+            na_indeces = ys_pred[y_col].isnull()
+            imputed_y = self.imputers_[y_col].predict(x[na_indeces]).round()
+            imputed_y[imputed_y < 0] = 0
+            ys_pred.loc[na_indeces, y_col] = imputed_y
+
+        return ys_pred
